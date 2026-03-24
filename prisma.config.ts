843a5fd44f461893099b9ceee180abcd08ed6c91
Yisuscrist72 <jesus.lopeztavio72@gmail.com>
2026-03-24 09:39:03 +0000

    .gitignore

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'prisma/config';
-import 'dotenv/config';
+import { config } from 'dotenv';
+import path from 'path';
+
+config({ path: path.join(process.cwd(), 'prisma', '.env') });
 
 export default defineConfig({
     datasource: {
